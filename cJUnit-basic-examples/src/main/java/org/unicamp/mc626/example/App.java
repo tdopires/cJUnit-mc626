b0b46768eed3b7bd548b0754608e92ba3ae8938c
Thiago de Oliveira Pires <thiago.op93@gmail.com>
2014-06-23 21:35:40 -0300

    Exemplos básicos

---
+++ cJUnit-basic-examples/src/main/java/org/unicamp/mc626/example/App.java
@@ -0,0 +1,13 @@
+package org.unicamp.mc626.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
